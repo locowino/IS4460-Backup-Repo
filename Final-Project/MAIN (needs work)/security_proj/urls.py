dc0db9a66b0046b8ea15a618ec60b558db3ec1dd
Loc Nguyen <locsnip@gmail.com>
2024-04-11 15:12:25 -0600

    Logout Button but is not working :,(

--- Final-Project/MAIN (needs work)/security_proj/urls.py
+++ Final-Project/MAIN (needs work)/security_proj/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/',include('customer.urls')),
-    path("accounts/", include("django.contrib.auth.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('logout/', auth_views.LogoutView.as_view(next_page= '/accounts/login/'), name = 'logout'),
 ]
