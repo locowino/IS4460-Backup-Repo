f01a3cf171fc6ec5f849d36c678f338b40c0b04e
Ashley Dabb <ashleyadabb@gmail.com>
2024-04-11 21:51:13 -0600

    trying to get shopping cart and db working

--- Final-Project/MAIN (needs work)/security_proj/urls.py
+++ Final-Project/MAIN (needs work)/security_proj/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("accounts/", include("django.contrib.auth.urls")),
     path('logout/', auth_views.LogoutView.as_view(next_page= '/accounts/login/'), name = 'logout'),
+    
 ]
 
--- lab_7_orig/customer_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CustomerAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'customer_app'
