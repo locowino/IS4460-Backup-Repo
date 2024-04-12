9f2713eb7ac546c1f9c01bf47eac52f24612f01c
Ashley Dabb <ashleyadabb@gmail.com>
2024-04-11 20:09:31 -0600

    customer problems

--- Final-Project/MAIN (needs work)/customer/views.py
+++ Final-Project/MAIN (needs work)/customer/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from .models import Customer
+from .models import Main
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your class based views here.
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 class CustomerListView(LoginRequiredMixin, View):
 
     def get(self,request):
-        main_page = Customer.objects.all()
+        main_page = Main.objects.all()
         return render(request=request,
                       template_name='main_page/main_page.html',
                       context = {'main_page':main_page}
--- Final-Project/MAIN (needs work)/security_proj/urls.py
+++ Final-Project/MAIN (needs work)/security_proj/urls.py
@@ -3,15 +3,16 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
-def movie_list(request):
-    return HttpResponse("This is the movies list page.")
+
+#def movie_list(request):
+    #return HttpResponse("This is the movies list page.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/',include('customer.urls')),
-    path('movies/', movie_list, name='movie-list'),
+    #path('movies/', movie_list, name='movie-list'),
 
     path("accounts/", include("django.contrib.auth.urls")),
     path('logout/', auth_views.LogoutView.as_view(next_page= '/accounts/login/'), name = 'logout'),
