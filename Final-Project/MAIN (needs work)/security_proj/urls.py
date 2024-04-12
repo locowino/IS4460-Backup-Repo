1491f2536cddcff17aedd059f1bdec8844b87d7b
Ashley Dabb <ashleyadabb@gmail.com>
2024-04-11 20:43:27 -0600

    logout button working

--- Final-Project/MAIN (needs work)/security_proj/urls.py
+++ Final-Project/MAIN (needs work)/security_proj/urls.py
@@ -7,7 +7,7 @@ from django.shortcuts import render
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('customer/',include('customer.urls')),
-    
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("accounts/", include("django.contrib.auth.urls")),
     path('logout/', auth_views.LogoutView.as_view(next_page= '/accounts/login/'), name = 'logout'),
 ]
